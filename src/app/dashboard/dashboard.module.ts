c250d3422834ca1bdec51e89cc7c3941d72b9e42
wiliansaugusto <wiliansaugusto@gmail.com>
2021-06-07 10:42:01 -0300

    Primeira Versão do Front

---
+++ src/app/dashboard/dashboard.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
+import { DashboardPageComponent } from './dashboard-page/dashboard-page.component';
+import { StockCardComponent } from './stock-card/stock-card.component';
+
+
+
+@NgModule({
+  declarations: [
+    DashboardPageComponent,
+    StockCardComponent
+  ],
+  imports: [
+    CommonModule,
+    HttpClientModule,
+  ],
+  exports:[
+    DashboardPageComponent
+  ],
+})
+export class DashboardModule { }
