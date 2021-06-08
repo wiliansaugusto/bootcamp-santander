70fe0a482974ae0a985602f3e818a1de12190135
wiliansaugusto <wiliansaugusto@gmail.com>
2021-06-08 19:50:12 -0300

    Adicionando o salvar  stock

--- src/app/dashboard/dashboard.service.ts
+++ src/app/dashboard/dashboard.service.ts
@@ -1,16 +1,17 @@
 import { DatePipe } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
 import Stock from '../shared/models/stock-model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DashboardService {
-  private datepipe: DatePipe
-readonly baseUrl = "https://bootcamp-dio-santander-willian.herokuapp.com/bootcamp";
-  constructor(private http:HttpClient, ) { }
+
+
+  readonly baseUrl = "https://bootcamp-dio-santander-willian.herokuapp.com/bootcamp";
+  constructor(private http:HttpClient ) { }
+  protected datePipe: DatePipe = new DatePipe('pt-BR')
 
   async getStocks():Promise<Stock[]>{
     return this.http.get<Stock[]>(`${this.baseUrl}/stock`).toPromise();
@@ -23,14 +24,14 @@ readonly baseUrl = "https://bootcamp-dio-santander-willian.herokuapp.com/bootcam
     return this.http.delete(`${this.baseUrl}/stock/${id}`).toPromise();
 
   }
-  async saveStock(stock:Stock):Promise <any>{
+   saveStock(stock:Stock):Promise <any>{
     stock.id = null;
-    let data = new Date(stock.date);
-    let dataFormatada = ((data.getDate() )) + "/" + ((data.getMonth() + 1))
-     + "/" + data.getFullYear();
-    stock.date = dataFormatada;
+     let datanoram = new Date(stock.date);
+     let latest_date =this.datePipe.transform(datanoram, 'dd/MM/yyyy');
+    stock.date = latest_date;
     console.log(stock);
-    return this.http.post(`${this.baseUrl}/stock/`,JSON.stringify(stock) ).toPromise();
+
+    return this.http.post(`${this.baseUrl}/stock/`,stock ).toPromise();
   }
 
 }
